9d0fced2603faccd922c8434a1d3dce3e35783ac
ElliotRedhead <elliot.redhead@gmail.com>
2024-05-31 00:36:05 +0100

    Configure harpoon and telescope

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+        -- This will not install any breaking changes.
+        -- For major updates, this must be adjusted manually.
+        version = "^1.0.0",
+      },
+    },
+    opts = function(_, opts)
+      local lga_actions = require("telescope-live-grep-args.actions")
+      opts.extensions = {
+        live_grep_args = {
+          auto_quoting = true, -- enable/disable auto-quoting
+          -- define mappings, e.g.
+          mappings = { -- extend mappings
+            i = {
+              ["<C-k>"] = lga_actions.quote_prompt(),
+              ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+            },
+          },
+          -- ... also accepts theme settings, for example:
+          -- theme = "dropdown", -- use dropdown theme
+          -- theme = { }, -- use own theme spec
+          -- layout_config = { mirror=true }, -- mirror preview pane
+        },
+      }
+    end,
+    keys = {
+      {
+        "<leader>/",
+        "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+        desc = "Grep (root dir)",
+      },
+    },
+    config = function(_, opts)
+      local telescope = require("telescope")
+
+      -- first setup telescope
+      telescope.setup(opts)
+
+      -- then load the extension
+      telescope.load_extension("live_grep_args")
+    end,
+  },
+}
